3e48a757007eb4ba1de31edfe7a128cf6405ec3b
Nghia Vu <Nghia.vu1010@gmail.com>
2019-09-12 17:46:50 -0400

    Setup server file

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+// These are modules that are installed from NPM and are imported into this file
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require("hbs");
 
+// Create routes (or a file path) to the js files that will contain AJAX requests
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var leaguesRouter = require('./routes/leagues');
 
+// Connect express to the APP
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -19,8 +24,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Connect the handlebars partials to be used with views
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/leagues', leaguesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
